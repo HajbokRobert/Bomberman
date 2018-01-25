3cf2b447934078a69d01df12da0f2f6774aecde9
Hajbok Robert <hajbok.robert@gmail.com>
2018-01-25 16:32:59 +0000

    Heart new vector png

--- Bomberman/wwwroot/js/prefabs/Lives.js
+++ Bomberman/wwwroot/js/prefabs/Lives.js
@@ -8,11 +8,11 @@ Bomberman.Lives = function (game_state, name, position, properties) {
     this.fixedToCamera = true;
 
     this.anchor.setTo(0.5);
-    this.scale.setTo(0.9);
+    this.scale.setTo(0.6);
 
     // create a text prefab to show the number of lives
-    lives_text_position = new Phaser.Point(this.position.x - 2, this.position.y + 5);
-    lives_text_style = { font: "14px Arial", fill: "#fff" };
+    lives_text_position = new Phaser.Point(this.position.x, this.position.y + 2);
+    lives_text_style = { font: "10px Arial", fill: "#fff" };
     lives_text_properties = { group: "hud", text: this.number_of_lives, style: lives_text_style };
     this.lives_text = new Bomberman.TextPrefab(this.game_state, "lives_text", lives_text_position, lives_text_properties);
     this.lives_text.anchor.setTo(0.5);
